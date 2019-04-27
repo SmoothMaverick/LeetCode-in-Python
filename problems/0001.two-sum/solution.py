88687083efc733cb75f9ce376d850d5189a4e8d2
jason <5660582+SmoothMaverick@users.noreply.github.com>
2019-04-27 16:43:33 -0700

    add 0001 Two Sum solution

--- problems/0001.two-sum/solution.py
+++ problems/0001.two-sum/solution.py
@@ -3,4 +3,25 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pass
+        seen = {}
+
+        for i, v in enumerate(nums):
+            diff = target - v
+
+            if diff in seen:
+                j = seen[diff]
+                return [j, i]
+
+            seen[v] = i
+
+        return []
+
+
+class BruteForce:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+        return []
--- problems/0001.two-sum/solution_test.py
+++ problems/0001.two-sum/solution_test.py
@@ -1,16 +1,30 @@
 import unittest
 from solution import Solution
+from solution import BruteForce
 
 
 class TestTwoSum(unittest.TestCase):
     def setUp(self):
-        self.test_cases = [{"nums": [2, 7, 11, 15], "target": 9}]
+        self.test_cases = [
+            {"nums": [2, 7, 11, 15], "target": 9, "answer": [0, 1]},
+            {"nums": [1, 8, 13, 38], "target": 21, "answer": [1, 2]},
+            {"nums": [10, 1, 25, 99], "target": 100, "answer": [1, 3]},
+            {"nums": [0, 0], "target": 0, "answer": [0, 1]},
+        ]
 
-    def test_two_sum(self):
+    def test_solution(self):
         soln = Solution()
-        for test in self.test_cases:
-            result = soln.twoSum(test["nums"], test["target"])
-            self.assertEqual(result, 9)
+        for i, test in enumerate(self.test_cases):
+            with self.subTest(i=i):
+                result = soln.twoSum(test["nums"], test["target"])
+                self.assertEqual(result, test["answer"])
+
+    def test_brute_force(self):
+        soln = BruteForce()
+        for i, test in enumerate(self.test_cases):
+            with self.subTest(i=i):
+                result = soln.twoSum(test["nums"], test["target"])
+                self.assertEqual(result, test["answer"])
 
 
 if __name__ == "__main__":
