e00d87a1f99fab09d07ac7cd3379a13490ac6379
jason <5660582+SmoothMaverick@users.noreply.github.com>
2019-05-02 00:11:56 -0700

    add 0707 Design Linked List solution

--- designs/0707.design-linked-list/solution.py
+++ designs/0707.design-linked-list/solution.py
@@ -6,22 +6,51 @@ class MyLinkedList:
         """
         Initialize your data structure here.
         """
+        self.head = None
+        self.tail = None
 
     def get(self, index: int) -> int:
         """
         Get the value of the index-th node in the linked list. If the index is invalid, return -1.
         """
+        curr = self.head
+
+        for i in range(index + 1):
+            if curr == None:
+                break
+
+            if i == index:
+                return curr.val
+
+            curr = curr.next
+
+        return -1
 
     def addAtHead(self, val: int) -> None:
         """
         Add a node of value val before the first element of the linked list.
         After the insertion, the new node will be the first node of the linked list.
         """
+        node = MyNode(val)
+        node.next = self.head
+
+        if self.head == None:
+            self.tail = node
+
+        self.head = node
 
     def addAtTail(self, val: int) -> None:
         """
         Append a node of value val to the last element of the linked list.
         """
+        if self.head == None:
+            self.addAtHead(val)
+            return
+
+        node = MyNode(val)
+
+        self.tail.next = node
+        self.tail = node
 
     def addAtIndex(self, index: int, val: int) -> None:
         """
@@ -29,9 +58,60 @@ class MyLinkedList:
         If index equals to the length of linked list, the node will be appended to the end
         of linked list. If index is greater than the length, the node will not be inserted.
         """
+        if index <= 0:
+            self.addAtHead(val)
+            return
+
+        curr = self.head
+        prev = None
+
+        for i in range(index + 1):
+            if curr == None:
+                if i == index:
+                    self.addAtTail(val)
+                    return
+                else:
+                    return
+
+            if i == index:
+                node = MyNode(val)
+                node.next = curr
+                prev.next = node
+                return
+
+            prev = curr
+            curr = curr.next
 
     def deleteAtIndex(self, index: int) -> None:
         """
         Delete the index-th node in the linked list, if the index is valid.
         """
+        if self.head == None:
+            return
 
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        curr = self.head
+        prev = None
+
+        for i in range(index + 1):
+            if curr == None:
+                return
+
+            if i == index:
+                if curr == self.tail:
+                    self.tail = prev
+
+                prev.next = curr.next
+                return
+
+            prev = curr
+            curr = curr.next
+
+
+class MyNode:
+    def __init__(self, val: int):
+        self.val = val
+        self.next = None
--- designs/0707.design-linked-list/solution_test.py
+++ designs/0707.design-linked-list/solution_test.py
@@ -1,17 +1,86 @@
-import unittest
+import pytest
+
 from solution import MyLinkedList
 
+test_cases = [
+    [
+        ["addAtHead", [1], None],
+        ["get", [0], 1],
+        ["addAtTail", [3], None],
+        ["get", [1], 3],
+        ["addAtIndex", [1, 2], None],
+        ["get", [1], 2],
+        ["deleteAtIndex", [1], None],
+        ["get", [1], 3],
+    ],
+    [
+        ["get", [0], -1],
+        ["addAtIndex", [1, 2], None],
+        ["get", [1], -1],
+        ["addAtIndex", [0, 1], None],
+        ["get", [0], 1],
+    ],
+    [["addAtHead", [1], None], ["deleteAtIndex", [0], None]],
+    [
+        ["addAtHead", [1], None],
+        ["addAtTail", [3], None],
+        ["addAtIndex", [1, 2], None],
+        ["get", [1], 2],
+        ["deleteAtIndex", [0], None],
+        ["get", [0], 2],
+    ],
+    [
+        ["addAtIndex", [0, 10], None],
+        ["addAtIndex", [0, 20], None],
+        ["addAtIndex", [1, 30], None],
+        ["get", [0], 20],
+    ],
+    [["addAtIndex", [-1, 0], None], ["get", [0], 0], ["deleteAtIndex", [-1], None]],
+    [
+        ["addAtHead", [1], None],
+        ["addAtTail", [3], None],
+        ["addAtIndex", [4, 2], None],
+        ["get", [1], 3],
+        ["deleteAtIndex", [-1], None],
+        ["get", [1], 3],
+    ],
+    [
+        ["addAtHead", [8], None],
+        ["get", [1], -1],
+        ["addAtTail", [81], None],
+        ["deleteAtIndex", [2], None],
+        ["addAtHead", [26], None],
+        ["deleteAtIndex", [2], None],
+        ["get", [0], 26],
+        ["get", [1], 8],
+        ["addAtTail", [24], None],
+        ["get", [0], 26],
+        ["get", [1], 8],
+        ["get", [2], 24],
+        ["addAtHead", [15], None],
+        ["addAtTail", [0], None],
+        ["addAtTail", [13], None],
+        ["addAtTail", [1], None],
+        ["addAtIndex", [6, 33], None],
+        ["get", [0], 15],
+        ["get", [1], 26],
+        ["get", [2], 8],
+        ["get", [3], 24],
+        ["get", [4], 0],
+        ["get", [5], 13],
+        ["get", [6], 33],
+        ["get", [7], 1],
+    ],
+]
 
-class TestDesignLinkedList(unittest.TestCase):
-    def runTest(self):
-        obj = MyLinkedList([])
-        obj.addAtHead(1)
-        obj.addAtTail(3)
-        obj.addAtIndex(1, 2)
-        self.assertEqual(obj.get(1), 2)
-        obj.deleteAtIndex(1)
-        self.assertEqual(obj.get(1), 3)
 
+@pytest.mark.parametrize("test_case", test_cases)
+def test_linked_list(test_case):
+    obj = MyLinkedList()
 
-if __name__ == "main":
-    unittest.main()
+    for test in test_case:
+        func = test[0]
+        param = test[1]
+        expected = test[2]
+
+        assert getattr(obj, func)(*param) == expected
