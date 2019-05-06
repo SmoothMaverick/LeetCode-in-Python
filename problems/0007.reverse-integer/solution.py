f16fefa0fae905426f6ff133340b02c38785090f
jason <5660582+SmoothMaverick@users.noreply.github.com>
2019-05-05 18:58:54 -0700

    add 0007 Reverse Integer solution

--- problems/0007.reverse-integer/solution.py
+++ problems/0007.reverse-integer/solution.py
@@ -1,3 +1,16 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        pass
+        if x > 0:
+            y = str(x)
+            x = int(y[::-1])
+        elif x < 0:
+            y = str(-1 * x)
+            x = int(y[::-1])
+            x = x * -1
+        else:
+            return 0
+
+        if (x > 2 ** 31 - 1) or (x < -2 ** 31):
+            return 0
+
+        return x
