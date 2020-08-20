12ca6e954fcab3c9753bf87f33b9fd28728c5cbf
jason <5660582+SmoothMaverick@users.noreply.github.com>
2020-08-20 15:59:47 -0700

    update 0007 Reverse Integer solution
    Use a more fundamental solution to swap values instead of an abstract
    python function.
    
    Add brute force solution that is easier to understand and remember.

--- problems/0007.reverse-integer/solution.py
+++ problems/0007.reverse-integer/solution.py
@@ -1,16 +1,65 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x > 0:
-            y = str(x)
-            x = int(y[::-1])
-        elif x < 0:
-            y = str(-1 * x)
-            x = int(y[::-1])
-            x = x * -1
-        else:
-            return 0
+        """
+            https://leetcode.com/problems/reverse-integer/discuss/798411/Python-3-O(n)-Time-O(1)-Space-with-explanation
 
-        if (x > 2 ** 31 - 1) or (x < -2 ** 31):
+            1.  set sign as 1 and if x is less than 0, change sign as -1
+            2.  turn it into string and if x < 0 then trim it off index 0: x = x[1:]
+            3.  turn x into a list so you can do in place replacement for constant memory space
+            4.  Use while loop and two pointer to switch the digits
+            5.  turn the list into string by using ''.join(x)
+            6.  convert x back to integer and multiply the sign
+            7.  If x is not within [-2**31, 2**31-1], return 0
+            8.  Otherwise return x
+
+            Time complexity: O(n)
+            Space complexity: O(1)
+        """
+
+        sign = 1
+
+        if x < 0:
+            sign = -1
+            x = str(x)[1:]
+        else:
+            x = str(x)
+
+        x = list(x)
+
+        left = 0
+        right = len(x) - 1
+
+        while left < right:
+            x[left], x[right] = x[right], x[left]
+            left += 1
+            right -= 1
+
+        x = "".join(x)
+        x = sign * int(x)
+
+        if x < -(2 ** 31) or x > 2 ** 31 - 1:
             return 0
 
         return x
+
+
+class BruteForce:
+    def reverse(self, x: int) -> int:
+        value = ""
+        sign = ""
+
+        x = str(x)
+
+        if x[0] == "-":
+            sign = "-"
+            x = x[1:]
+
+        for num in x:
+            value = num + value
+
+        value = int(sign + value)
+
+        if value > 2 ** 31 - 1 or value < -(2 ** 31):
+            return 0
+
+        return value
--- problems/0007.reverse-integer/solution_test.py
+++ problems/0007.reverse-integer/solution_test.py
@@ -1,11 +1,12 @@
 import pytest
 
-from solution import Solution
+from solution import Solution, BruteForce
 
 
 @pytest.mark.parametrize(
     "test_input,expected",
-    [(123, 321), (-123, -321), (120, 21), (1463847412, 2147483641)],
+    [(123, 321), (-123, -321), (120, 21), (1463847412, 2147483641), (1534236469, 0)],
 )
 def test_revese(test_input, expected):
     assert Solution().reverse(test_input) == expected
+    assert BruteForce().reverse(test_input) == expected
