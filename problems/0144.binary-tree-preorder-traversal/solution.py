f6ee73d69ff2de25fffa83160a0d7c32d92bdda4
jason <5660582+SmoothMaverick@users.noreply.github.com>
2021-01-21 13:37:09 -0800

    add 0144 Binary Tree Preorder Traversal solutions
    Recursive and iterative solutions

--- problems/0144.binary-tree-preorder-traversal/solution.py
+++ problems/0144.binary-tree-preorder-traversal/solution.py
@@ -1,12 +1,50 @@
 from typing import List
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 
-class Solution:
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Recursive:
     def preorderTraversal(self, root: TreeNode) -> List[int]:
-        pass
+        output = []
+
+        def traverse(root: TreeNode):
+            output.append(root.val)
+
+            if root.left:
+                traverse(root.left)
+
+            if root.right:
+                traverse(root.right)
+
+            return
+
+        if root:
+            traverse(root)
+
+        return output
+
+
+class Iterative:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        output = []
+        queue = []
+
+        if root:
+            queue.append(root)
+
+        while queue:
+            root = queue.pop()
+            output.append(root.val)
+
+            if root.right:
+                queue.append(root.right)
+
+            if root.left:
+                queue.append(root.left)
+
+        return output
--- problems/0144.binary-tree-preorder-traversal/solution_test.py
+++ problems/0144.binary-tree-preorder-traversal/solution_test.py
@@ -1,16 +1,47 @@
 import pytest
-from solution import Solution
+from solution import Recursive, Iterative, TreeNode
+
+test_data = [
+    ([1, None, 2, 3], [1, 2, 3]),
+    ([], []),
+    ([1], [1]),
+    ([1, 2], [1, 2]),
+    ([1, None, 2], [1, 2]),
+    ([3, 1, 2], [3, 1, 2]),
+    ([2, 1, 3, None, 4], [2, 1, 4, 3]),
+]
 
 
-@pytest.mark.parametrize(
-    "test_input,expected",
-    [
-        ([1, null, 2, 3], [1, 2, 3]),
-        ([], []),
-        ([1], [1]),
-        ([1, 2], [1, 2]),
-        ([1, null, 2], [1, 2]),
-    ],
-)
-def test_revese(test_input, expected):
-    assert Solution().preorderTraversal(test_input) == expected
+def makeTree(input) -> TreeNode:
+    root = None
+    queue = []
+
+    if len(input) > 0:
+        root = TreeNode(input[0])
+        queue.append(root)
+
+        for i in range(1, len(input), 2):
+            j = i + 1
+            node = queue.pop()
+
+            if len(input) > j and input[j]:
+                node.right = TreeNode(input[j])
+                queue.append(node.right)
+
+            if input[i]:
+                node.left = TreeNode(input[i])
+                queue.append(node.left)
+
+    return root
+
+
+@pytest.mark.parametrize("test_input,expected", test_data)
+def test_recursive(test_input, expected):
+    root = makeTree(test_input)
+    assert Recursive().preorderTraversal(root) == expected
+
+
+@pytest.mark.parametrize("test_input,expected", test_data)
+def test_iterative(test_input, expected):
+    root = makeTree(test_input)
+    assert Iterative().preorderTraversal(root) == expected
